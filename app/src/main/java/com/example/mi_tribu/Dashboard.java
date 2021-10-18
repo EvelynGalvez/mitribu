2416108e8c5c0fdf97f42b4a83826ad77fb90374
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-10-17 22:49:58 -0300

    form_sales

--- app/src/main/java/com/example/mi_tribu/Dashboard.java
+++ app/src/main/java/com/example/mi_tribu/Dashboard.java
@@ -2,13 +2,29 @@ package com.example.mi_tribu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.Normalizer;
 
 public class Dashboard extends AppCompatActivity {
+    private Button btn_prueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        btn_prueba = findViewById(R.id.btn_prueba);
+
+        btn_prueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Form_sales.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mi_tribu/Form_sales.java
@@ -0,0 +1,14 @@
+package com.example.mi_tribu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_sales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_sales);
+    }
+}
