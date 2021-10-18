e35ddf7c0ab8db497d1e834a71b7a222bf30a8a5
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-10-18 18:45:40 -0300

    Estilos a botones y se agrega boton imagen

--- app/src/main/java/com/example/mi_tribu/Dashboard.java
+++ app/src/main/java/com/example/mi_tribu/Dashboard.java
@@ -6,20 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.text.Normalizer;
 
 public class Dashboard extends AppCompatActivity {
-    private Button btn_prueba;
+    private Button img_porteo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        btn_prueba = findViewById(R.id.btn_prueba);
+        ImageButton ib=(ImageButton)findViewById(R.id.img_porteo);
 
-        btn_prueba.setOnClickListener(new View.OnClickListener() {
+        ib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), Form_sales.class);
