23ee8f045aa54e705027520ea9dca233631afb5a
EvelynGalvez <evelyn.galvez.p@gmail.com>
2021-10-18 22:51:14 -0300

    interfaz ventas

--- app/src/main/java/com/example/mi_tribu/Form_sales.java
+++ app/src/main/java/com/example/mi_tribu/Form_sales.java
@@ -2,13 +2,27 @@ package com.example.mi_tribu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Form_sales extends AppCompatActivity {
+    private Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_sales);
+        btn_send = findViewById(R.id.btn_send);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Sales.class);
+                startActivity(intent);
+            }
+        });
     }
 }
